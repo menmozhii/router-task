9f11407151872d7c12fcb9edd106f3184c2de7d1
menmozhii <mozhieee@gmail.com>
2024-03-06 23:33:40 +0530

    guvi main

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ function About() {
 <h1 className='about' >welcome to guvi blog</h1>
 <div className="all-card">
     <div className="card" style={{"width":"18rem"}}>
-  <img src="src\image\about.jpeg" className="card-img-center" alt="..." width="250" height="300"/>
-  
+  <img src="src\image\guvi main.png" className="card-img-center" alt="..." width="250" height="300"/>
+  {/* E:\React_51\router_task\src\image\about.jpeg */}
 </div>
 </div>
   </>
